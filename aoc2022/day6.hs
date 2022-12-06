41fdf55d8ed864d60569a0350d098ee34a0b5dcf
Kristian Sørdal <krisor99@gmail.com>
2022-12-06 07:05:57 +0100

    :sparkles: day 6 finished

--- aoc2022/day6.hs
+++ aoc2022/day6.hs
@@ -1,16 +1,13 @@
-import Data.Set (Set)
-import Data.Set qualified as Set
+import Data.List
 
 main = do
   input <- head . lines <$> readFile "input/day6.in"
   let ansP1 = getMarker input 0 4
   let ansP2 = getMarker input 0 14
-  print ansP1
-  print ansP2
+  putStrLn $ "Part 1: " ++ show ansP1
+  putStrLn $ "Part 2: " ++ show ansP2
 
 getMarker :: String -> Int -> Int -> Int
-getMarker m c x =
-  let s = Set.fromList (take x m)
-   in if length s == x
-        then c + x
-        else getMarker (drop 1 m) (c + 1) x
+getMarker m c x
+  | length (nub (take x m)) == x = c + x
+  | otherwise = getMarker (drop 1 m) (c + 1) x
