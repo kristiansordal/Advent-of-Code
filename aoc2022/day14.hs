3609a6465b7372dd0873bb997e1ee7621895fe68
Kristian Sørdal <krisor99@gmail.com>
2022-12-14 19:47:12 +0100

    :sparkles: slow ass solution

--- aoc2022/day14.hs
+++ aoc2022/day14.hs
@@ -7,39 +7,24 @@ main = do
   input <- map (map parseTupParen . splitOn "->") . lines <$> readFile "input/day14.in"
   let set = Set.fromList (concat $ world input [])
       abyss = maximum $ map snd (concat input)
-      ansP1 = sand set (500, 0) abyss 0
-      ansP2 = sand' set (500, 0) abyss 0
+      ansP1 = sand set (500, 0) abyss 0 False
+      ansP2 = sand set (500, 0) abyss 0 True
   print ansP1
   print ansP2
 
-sand :: Set (Integer, Integer) -> (Integer, Integer) -> Integer -> Integer -> Integer
-sand coords pos a c
+sand :: Set (Integer, Integer) -> (Integer, Integer) -> Integer -> Integer -> Bool -> Integer
+sand coords pos a c p
+  | Set.member (500, 0) coords && p = c
   | Set.member next coords =
       if Set.member left coords
         then
           if Set.member right coords
-            then sand (Set.insert pos coords) (500, 0) a (c + 1)
-            else sand coords right a c
-        else sand coords left a c
-  | snd next > a = c
-  | otherwise = sand coords next a c
-  where
-    next = (fst pos, snd pos + 1)
-    right = (fst pos + 1, snd pos + 1)
-    left = (fst pos - 1, snd pos + 1)
-
-sand' :: Set (Integer, Integer) -> (Integer, Integer) -> Integer -> Integer -> Integer
-sand' coords pos a c
-  | Set.member (500, 0) coords = c
-  | Set.member next coords =
-      if Set.member left coords
-        then
-          if Set.member right coords
-            then sand' (Set.insert pos coords) (500, 0) a (c + 1)
-            else sand' coords right a c
-        else sand' coords left a c
-  | snd next == a + 2 = sand' (Set.insert pos coords) (500, 0) a (c + 1)
-  | otherwise = sand' coords next a c
+            then sand (Set.insert pos coords) (500, 0) a (c + 1) p
+            else sand coords right a c p
+        else sand coords left a c p
+  | snd next > a && not p = c
+  | snd next == a + 2 && p = sand (Set.insert pos coords) (500, 0) a (c + 1) p
+  | otherwise = sand coords next a c p
   where
     next = (fst pos, snd pos + 1)
     right = (fst pos + 1, snd pos + 1)
