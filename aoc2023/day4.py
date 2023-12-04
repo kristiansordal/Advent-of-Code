ed64bfb00077d26cbf65256a15b455bef4577eb4
Kristian Sørdal <krisor99@gmail.com>
2023-12-04 11:40:25 +0100

    day 4 - nice solution

---
+++ aoc2023/day4.py
@@ -0,0 +1,17 @@
+data = open("input/4.in").read().splitlines()
+score = 0
+hits, counts, cs, ws = [], [1] * len(data), [], []
+
+for i, l in enumerate(data):
+    text, card = l.split(":")[0], l.split(":")[1].split("|")
+    ws.append(list(map(int, card[0].split())))
+    cs.append(list(map(int, card[1].split())))
+    hits.append(sum(1 if x in ws[i] else 0 for x in cs[i]))
+    score += 2 ** (hits[i] - 1) if hits[i] > 0 else 0
+
+for i in range(len(cs)):
+    if counts[i] > 0:
+        for x in range(i + 1, i + 1 + hits[i]):
+            counts[x] += counts[i]
+
+print(f"P1: {int(score)}, P2: {sum(counts)}")
