b88b402c62f464253bd114c53eb27a1f4fcf76af
Kristian Sørdal <krisor99@gmail.com>
2023-12-06 12:52:52 +0100

    changed day 6 to use quadratic formula

--- aoc2023/day6.py
+++ aoc2023/day6.py
@@ -1,18 +1,13 @@
+from math import floor, ceil, prod
+
 D = open("input/6.in").read().splitlines()
 tds = list(zip(list(map(int, D[0].split()[1:])), list(map(int, D[1].split()[1:]))))
-time, dist = int((D[0].replace(" ", "")[5:])), int(D[1].replace(" ", "")[9:])
-tds.append((time, dist))
-p1, p2 = 1, 0
+tds.append((int((D[0].replace(" ", "")[5:])), int(D[1].replace(" ", "")[9:])))
 
-for i, (t, d) in enumerate(tds):
-    v, j, tc = 0, 0, t
-    while v * tc < d:
-        v += 1
-        tc -= 1
-        j += 1
-    if i < len(tds) - 1:
-        p1 *= (t - 2 * j + 1) if t % 2 == 1 else (t - 2 * j - 1)
-    else:
-        p2 = (t - 2 * j + 1) if t % 2 == 1 else (t - 2 * j - 1)
+res = []
+for t, d in tds:
+    x = (-t - (t**2 - 4 * d) ** 0.5) / -2
+    y = (-t + (t**2 - 4 * d) ** 0.5) / -2
+    res.append(ceil(x) - floor(y) - 1)
 
-print(f"P1: {p1}, P2: {p2}")
+print(f"P1: {prod(res[:-1])}, P2: {res[-1]}")
