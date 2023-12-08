9ccb1e7e7bc4b9b79f10f71d68354729ef6f01f4
Kristian Sørdal <krisor99@gmail.com>
2023-12-08 10:36:06 +0100

    day 8

--- aoc2023/day8.py
+++ aoc2023/day8.py
@@ -11,30 +11,25 @@ for i, e in enumerate(edges):
     g.add_edge(nodes[i], e[0])
     g.add_edge(nodes[i], e[1])
 
-p1, v, c = 0, set(), "AAA"
 
-while "ZZZ" not in v:
-    ns = list(g.neighbors(c))
-    if d[p1 % len(d)] == "L":
-        c = ns[0]
-    else:
-        c = ns[1 % len(ns)]
-    v.add(c)
-    p1 += 1
+def search(r, s):
+    ans, v = 0, set()
 
+    while s not in v:
+        ns = list(g.neighbors(r))
+        if d[ans % len(d)] == "L":
+            r = ns[0]
+        else:
+            r = ns[1 % len(ns)]
+        v.add(r)
+        v.add(r[-1])
+        ans += 1
+    return ans
+
+
+p1 = search("AAA", "ZZZ")
 start_nodes, p2 = [n for n in nodes if n[-1] == "A"], []
 for n in start_nodes:
-    s, c, v = 0, n, set()
-
-    while "Z" not in v:
-        ns = list(g.neighbors(c))
-        if d[s % len(d)] == "L":
-            c = ns[0]
-        else:
-            c = ns[1 % len(ns)]
-        v.add(c[-1])
-        s += 1
-
-    p2.append(s)
+    p2.append(search(n, "Z"))
 
 print(f"P1: {p1}, P2: {lcm(*p2)}")
